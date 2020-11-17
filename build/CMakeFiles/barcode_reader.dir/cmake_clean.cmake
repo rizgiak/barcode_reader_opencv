50d0cbba2805e9392cc5c3582fd4894e4c97ab70
Khilmi <auliakhilmirizgi@gmail.com>
2020-11-17 10:26:02 +0700

    Fix searching zbar package

---
+++ build/CMakeFiles/barcode_reader.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/barcode_reader.dir/main.cpp.o"
+  "barcode_reader.pdb"
+  "barcode_reader"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/barcode_reader.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
